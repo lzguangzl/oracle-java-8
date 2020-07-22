d07d17277dbac3ffbc01e7d895031b7c8f47a8ba
zguang <zguang.loh@gmail.com>
2020-07-23 01:31:34 +0800

    Java class structure

---
+++ JavaClassStructure/src/java/Employee.java
@@ -0,0 +1,82 @@
+package java;
+
+/* Class */
+public class Employee {
+
+    /* Variables */
+    private String firstName;
+    private String lastName;
+    private String mobileNumber;
+    private String homeAddress;
+    private String designation;
+    private int age;
+    private Double salary;
+
+    /* Constructor */
+    public Employee(String firstName, String lastName, String mobileNumber, String homeAddress, String designation, int age, Double salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.mobileNumber = mobileNumber;
+        this.homeAddress = homeAddress;
+        this.designation = designation;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    /* Methods */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public String getHomeAddress() {
+        return homeAddress;
+    }
+
+    public void setHomeAddress(String homeAddress) {
+        this.homeAddress = homeAddress;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# oracle-java
- Java SE 8 | To demonstrate knowledge of object-oriented concepts, the Java programming language and general knowledge of Java platforms and technologies.
+# Oracle Java 8
+
+## Purpose of the project
+To demonstrate knowledge of object-oriented concepts, the Java programming language and general knowledge of Java platforms and technologies.
+
+## Project Setup
+
+IDE(Integrated Development Environment)
+* [Intellij IDEA](https://www.jetbrains.com/idea/)
+
+Language
+* [Java SE 8](https://www.oracle.com/sg/java/technologies/javase/javase-jdk8-downloads.html)
+
+## Project Content
+#### Java Basics
+* Java class structure
+* Main method
+* Comments
+* Packages and imports
